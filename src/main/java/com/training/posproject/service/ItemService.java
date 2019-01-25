b3d799d20190c485333bd1e26923f26e6ffce4cb
Pasindu <pasinduath@gmail.com>
2019-01-25 06:43:01 +0530

    Implement item service interface

---
+++ src/main/java/com/training/posproject/service/ItemService.java
@@ -0,0 +1,9 @@
+package com.training.posproject.service;
+
+import com.training.posproject.util.Pair;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ItemService {
+     Pair getAllItems();
+}
