08bca8b52ad936415908068b8ba17eeb1061ff7a
Pasindu <pasinduath@gmail.com>
2019-01-25 06:44:50 +0530

    remove hardcoded config

--- src/main/java/com/training/posproject/PosprojectApplication.java
+++ src/main/java/com/training/posproject/PosprojectApplication.java
@@ -1,63 +1,17 @@
 package com.training.posproject;
 
-import com.training.posproject.model.Role;
-import com.training.posproject.model.User;
-import com.training.posproject.repository.ItemRepository;
-import com.training.posproject.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @SpringBootApplication(exclude = { RedisAutoConfiguration.class})
-public class PosprojectApplication implements CommandLineRunner {
+public class PosprojectApplication{
 
-    @Autowired
-    private UserRepository repository;
-    
-    @Autowired
-    private ItemRepository itemRepository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
 
     public static void main(String[] args) {
         SpringApplication.run(PosprojectApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-
-        repository.deleteAll();
-
-        // save a couple of Users
-        User newUser1 = new User("testUser1", "testU123",passwordEncoder.encode("catWoman"));
-        Set<Role> userRoles = new HashSet<Role>();
-        userRoles.add(Role.ROLE_ADMIN);
-        newUser1.setRoles(userRoles);
-        repository.save(newUser1);
-        repository.save(new User("testUser2", "testU456",passwordEncoder.encode("catWoman")));
-
-        // fetch all Users
-        System.out.println("User found with findAll():");
-        System.out.println("-------------------------------");
-        for (User user : repository.findAll()) {
-            System.out.println(user);
-        }
-        System.out.println();
-
-        // fetch an individual user
-        System.out.println("User found with findByUsername('testU123'):");
-        System.out.println("--------------------------------");
-        System.out.println(repository.findByUsername("testU123"));
-
-
-    }
 
 }
 
